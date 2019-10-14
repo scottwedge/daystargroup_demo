8d328a7695aac0fcbe9f3ae65ee74ff77c2e2460
steve <solise@mceesolutions.com>
2019-10-14 16:28:31 +0100

    vendor registration update

--- sunray/__manifest__.py
+++ sunray/__manifest__.py
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Sunray',
-    'version': '0.26',
+    'version': '0.27',
 
     # any module necessary for this one to work correctly
     'depends': ['base','hr','crm','hr_expense','project','purchase','helpdesk','stock','product','account_budget','purchase_requisition','mrp'],
