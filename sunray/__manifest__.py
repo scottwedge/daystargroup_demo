4f2e2e66cf15241a32d75bf6064f36c2d833bef5
steve <solise@mceesolutions.com>
2019-10-16 11:47:18 +0100

    product master data brand & item update

--- sunray/__manifest__.py
+++ sunray/__manifest__.py
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Sunray',
-    'version': '0.43',
+    'version': '0.44',
     # any module necessary for this one to work correctly
     'depends': ['base','hr','crm','sale','hr_expense','hr_holidays','project','purchase','helpdesk','stock','product','account_budget','purchase_requisition','mrp'],
 
