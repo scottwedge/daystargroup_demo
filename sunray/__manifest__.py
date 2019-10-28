3dc834093811dc3e1f8b3ba21c2282414577ce45
Michael Njoku <mnjoku@mceesolutions.com>
2019-10-28 08:13:40 +0100

    changed site code structure

--- sunray/__manifest__.py
+++ sunray/__manifest__.py
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Sunray',
-    'version': '0.68',
+    'version': '0.69',
     # any module necessary for this one to work correctly
     'depends': ['base','hr','crm','sale','hr_expense','hr_holidays','project','purchase','helpdesk','stock','sale_subscription','product','account_budget','purchase_requisition','mrp'],
 
