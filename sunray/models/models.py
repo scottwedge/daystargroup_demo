8555bac2ef39883904d1c4f30c607170143e4409
steve <solise@mceesolutions.com>
2020-01-16 05:50:09 +0100

    crm adjustment

--- sunray/models/models.py
+++ sunray/models/models.py
@@ -56,7 +56,7 @@ class Lead(models.Model):
     contract_duration = fields.Float(string='Contract Duration (year)')
     coordinates = fields.Char(string='Coordinates')
     
-    type_of_offer = fields.Selection([('lease_to_own', 'Lease to Own'), ('pass_battery', 'PaaS Battery'), 
+    type_of_offer = fields.Selection([('lease_to_own', 'Lease to own'), ('pass_battery', 'PaaS Battery'), 
                                       ('pass_diesel', 'PaaS Diesel'), ('solar', 'Solar Only'),('saas', 'SaaS'), ('sale', 'Sale')], string='Service Type', required=False,default='saas')
     #atm_power_at_night = fields.Selection([('yes', 'Yes'), ('no', 'No'),], string='Does the system power ATM night/we?', required=False,default='yes')
     
